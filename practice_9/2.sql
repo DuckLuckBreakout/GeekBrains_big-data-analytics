72caec6caf60adeb052e7219eef2e51fdb65f845
DuckLuckBreakout <mrdivan07@gmail.com>
2019-12-14 12:54:03 +0300

    Add task 2

--- practice_9/2.sql
+++ practice_9/2.sql
@@ -9,4 +9,22 @@ CREATE USER shop IDENTIFIED WITH sha256_password BY 'DASDK313>124.daas2';
 GRANT ALL ON shop TO 'shop';
 GRANT SELECT ON shop.* TO 'shop_read';
 
+/*
+2) (по желанию) Пусть имеется таблица accounts содержащая три столбца id, name, password, 
+   содержащие первичный ключ, имя пользователя и его пароль. Создайте представление username таблицы accounts, 
+   предоставляющий доступ к столбца id и name. Создайте пользователя user_read, который бы не имел доступа к таблице accounts, 
+   однако, мог бы извлекать записи из представления username.
+*/
 
+CREATE TABLE accounts(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    password VARCHAR(255)
+    );
+
+DROP VIEW IF EXISTS username;
+CREATE VIEW username AS SELECT id, name FROM accounts;
+
+
+CREATE USER user_read IDENTIFIED WITH sha256_password BY 'DASDK313>124.daas3';
+GRANT SELECT ON username TO 'user_read';
