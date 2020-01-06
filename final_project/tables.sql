054e3df134e15bfb8895e148703d9579fb46f66e
DuckLuckBreakout <mrdivan07@gmail.com>
2020-01-06 20:20:13 +0300

    Add some keays

--- final_project/tables.sql
+++ final_project/tables.sql
@@ -1,16 +1,6 @@
 SHOW DATABASES;
 USE bitskins;
 
-ALTER TABLE profiles DROP FOREIGN KEY profiles_user_id_fk;
-ALTER TABLE specific_items_on_sale DROP FOREIGN KEY specific_items_on_sale_item_type_id_fk;
-ALTER TABLE specific_items_on_sale DROP FOREIGN KEY specific_items_on_sale_item_quality_id_fk;
-ALTER TABLE specific_items_on_sale DROP FOREIGN KEY specific_items_on_sale_item_type_id_fk;
-ALTER TABLE specific_items_on_sale DROP FOREIGN KEY specific_items_on_sale_bot_id_fk;
-ALTER TABLE sold_items DROP FOREIGN KEY sold_items_seller_id_fk;
-ALTER TABLE sold_items DROP FOREIGN KEY sold_items_buyer_id_fk;
-
-
-
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,  
@@ -79,21 +69,6 @@ CREATE TABLE sold_items (
 );
 
 
-ALTER TABLE sold_items
-  ADD CONSTRAINT sold_items_seller_id_fk
-    FOREIGN KEY (seller_id) REFERENCES users(id)
-      ON DELETE CASCADE,
-  ADD CONSTRAINT sold_items_buyer_id_fk
-    FOREIGN KEY (buyer_id) REFERENCES users(id)
-      ON DELETE CASCADE,
-  ADD CONSTRAINT sold_items_item_type_id_fk
-    FOREIGN KEY (item_type_id) REFERENCES items_types(id)
-      ON DELETE CASCADE,
-  ADD CONSTRAINT sold_items_item_quality_id_fk
-    FOREIGN KEY (item_quality_id) REFERENCES item_qualities(id)
-      ON DELETE CASCADE;
-
-
 DROP TABLE IF EXISTS images;
 CREATE TABLE images (
   id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -118,14 +93,6 @@ CREATE TABLE items_types (
 
 
 
-ALTER TABLE items_types
-  ADD CONSTRAINT items_types_image_id_fk
-    FOREIGN KEY (image_id) REFERENCES images(id)
-      ON DELETE CASCADE,
-  ADD CONSTRAINT items_types_app_id_fk
-    FOREIGN KEY (app_id) REFERENCES apps(id)
-      ON DELETE CASCADE;
-
 DROP TABLE IF EXISTS buy_orders;
 CREATE TABLE buy_orders (
   id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -137,10 +104,54 @@ CREATE TABLE buy_orders (
   created_at DATETIME DEFAULT NOW()
 );
 
+
+ALTER TABLE sold_items DROP FOREIGN KEY sold_items_seller_id_fk;
+ALTER TABLE sold_items DROP FOREIGN KEY sold_items_buyer_id_fk;
+ALTER TABLE sold_items DROP FOREIGN KEY sold_items_item_type_id_fk;
+ALTER TABLE sold_items DROP FOREIGN KEY sold_items_item_quality_id_fk;
+
+ALTER TABLE sold_items
+  ADD CONSTRAINT sold_items_seller_id_fk
+    FOREIGN KEY (seller_id) REFERENCES users(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT sold_items_buyer_id_fk
+    FOREIGN KEY (buyer_id) REFERENCES users(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT sold_items_item_type_id_fk
+    FOREIGN KEY (item_type_id) REFERENCES items_types(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT sold_items_item_quality_id_fk
+    FOREIGN KEY (item_quality_id) REFERENCES item_qualities(id)
+      ON DELETE CASCADE;
+
+
+ALTER TABLE items_types DROP FOREIGN KEY items_types_image_id_fk;
+ALTER TABLE items_types DROP FOREIGN KEY items_types_app_id_fk;
+
+
+ALTER TABLE items_types
+  ADD CONSTRAINT items_types_image_id_fk
+    FOREIGN KEY (image_id) REFERENCES images(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT items_types_app_id_fk
+    FOREIGN KEY (app_id) REFERENCES apps(id)
+      ON DELETE CASCADE;
+
+
+ALTER TABLE profiles DROP FOREIGN KEY profiles_user_id_fk;
+
+
 ALTER TABLE profiles
   ADD CONSTRAINT profiles_user_id_fk
     FOREIGN KEY (user_id) REFERENCES users(id)
       ON DELETE CASCADE;
+
+
+
+ALTER TABLE specific_items_on_sale DROP FOREIGN KEY specific_items_on_sale_item_type_id_fk;
+ALTER TABLE specific_items_on_sale DROP FOREIGN KEY specific_items_on_sale_item_quality_id_fk;
+ALTER TABLE specific_items_on_sale DROP FOREIGN KEY specific_items_on_sale_item_type_id_fk;
+ALTER TABLE specific_items_on_sale DROP FOREIGN KEY specific_items_on_sale_bot_id_fk;
 
 ALTER TABLE specific_items_on_sale
   ADD CONSTRAINT specific_items_on_sale_item_type_id_fk
@@ -157,6 +168,11 @@ ALTER TABLE specific_items_on_sale
       ON DELETE CASCADE;
 
 
+
+ALTER TABLE buy_orders DROP FOREIGN KEY buy_orders_seller_id_fk;
+ALTER TABLE buy_orders DROP FOREIGN KEY buy_orders_item_type_id_fk;
+ALTER TABLE buy_orders DROP FOREIGN KEY buy_orders_item_quality_id_fk;
+ALTER TABLE buy_orders DROP FOREIGN KEY buy_orders_app_id_id_fk;
 
 ALTER TABLE buy_orders
   ADD CONSTRAINT buy_orders_seller_id_fk
